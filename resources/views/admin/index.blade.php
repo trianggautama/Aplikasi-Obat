df10c7e0296eae0801560df5b5e7819a0603fd3e
tri angga utama <trianggategarutama@gmail.com>
2021-08-19 06:52:06 +0800

    fix pemusnahan obat

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,60 +15,6 @@
     </div>
 
     <div class="wrapper wrapper-content">
-        <div class="row">
-            <div class="col-md">
-                <div class="widget style1 navy-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-users fa-5x"></i>
-                        </div>
-                        <div class="col-8 text-right">
-                            <span>User Dinas Kesehatan</span>
-                            <h2 class="font-bold">3 User</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="widget style1 lazur-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-users fa-5x"></i>
-                        </div>
-                        <div class="col-8 text-right">
-                            <span> User Puskesmas </span>
-                            <h2 class="font-bold">30 User</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="widget style1 yellow-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-file-text fa-5x"></i>
-                        </div> 
-                        <div class="col-8 text-right">
-                            <span> Jenis Obat </span>
-                            <h2 class="font-bold">12</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div class="col-md"><div class="widget style1 red-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-truck fa-5x"></i>
-                        </div>
-                        <div class="col-8 text-right">
-                            <span> pendistribusian Obat </span>
-                            <h2 class="font-bold">12</h2>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
         <div class="row mt-3">
             <div class="col-md">
                 <div class="card">
